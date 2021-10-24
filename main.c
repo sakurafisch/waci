f98cd15e559c25fa4fa99b31372c3ce0f0d695aa
sakurafisch <sakurafisch@gmail.com>
2021-10-24 16:29:28 +0800

    feat: 第一节 设计

---
+++ main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <string.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+
+int token;
+char *src, *old_src;
+int poolsize;
+int line;
+
+void next() {
+    token = *src++;
+    return;
+}
+
+void expression(int level) {
+    // do nothing
+}
+
+void program() {
+    next();
+    while (token > 0) {
+        printf("token is: %c\n", token);
+        next();
+    }
+}
+
+int eval() {
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    int i, fd;
+
+    argc--;
+    argv++;
+
+    poolsize = 256 * 1024;
+    line = 1;
+
+    if ((fd = open(*argv, 0)) < 0) {
+        printf("could not open(%s)\n", *argv);
+        return -1;
+    }
+
+    if (!(src = old_src = malloc(poolsize))) {
+        printf("could not malloc(%d) for source area\n", poolsize);
+        return -1;
+    }
+
+    if ((i = read(fd, src, poolsize-1)) <= 0) {
+        printf("read() returned %d\n", i);
+        return -1;
+    }
+    src[i] = 0;
+    close(fd);
+
+    program();
+    return eval();
+}
